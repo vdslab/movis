0a44cbf2bd6db78746a27914e77ee3897d263091
FugaTakata <maple1213goo@gmail.com>
2021-11-04 11:22:51 +0900

    remove webpack-copy-plugin

--- next.config.js
+++ next.config.js
@@ -1,15 +1,4 @@
 /** @type {import('next').NextConfig} */
-
-const CopyPlugin = require("copy-webpack-plugin");
-
 module.exports = {
   reactStrictMode: true,
-  webpack: (config) => {
-    config.plugins.push(
-      new CopyPlugin({
-        patterns: ["prisma/dev.db"],
-      })
-    );
-    return config;
-  },
 };

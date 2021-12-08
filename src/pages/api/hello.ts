01e6c1fa9f5694da69720bcb777b2a69128a39fc
kanekyo1234 <kanekyohunter.0314@softbank.ne.jp>
2021-12-08 19:31:27 +0900

    add network

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,13 +1,21 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
 
+import prisma from "@/lib/prisma";
+
 type Data = {
   name: string;
 };
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: "John Doe" });
+  const data = await prisma.movie.findMany({
+    include: {
+      genres: true,
+    },
+  });
+
+  res.status(200).json(JSON.parse(JSON.stringify(data)));
 }

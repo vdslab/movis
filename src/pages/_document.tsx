b5b55ba2762ea934a1f315027fb4d374b41e99ba
FugaTakata <maple1213goo@gmail.com>
2021-12-08 21:29:07 +0900

    色々追加

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -33,6 +33,27 @@ export default class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP&display=swap"
             rel="stylesheet"
           />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/site.webmanifest" />
+          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#022c43" />
+          <meta name="msapplication-TileColor" content="#ffd700" />
+          <meta name="theme-color" content="#022c43" />
         </Head>
         <body>
           <Main />

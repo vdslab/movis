017715a103c1c365a8f67223569445889d7d4101
FugaTakata <maple1213goo@gmail.com>
2021-12-19 16:43:57 +0900

    応急処置

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -23,7 +23,7 @@ export const Sidebar = ({ drawerOpen, drawerToggle, window }) => {
   const { register, handleSubmit } = useForm();
   const router = useRouter();
   const [genres, setGenres] = useState([]);
-  const { selected, person } = useSelector((state) => state.app);
+  const { selected } = useSelector((state) => state.app);
   const dispatch = useDispatch();
 
   const drawerWidth = matchUpLg ? 280 : "100%";
@@ -86,7 +86,7 @@ export const Sidebar = ({ drawerOpen, drawerToggle, window }) => {
           </IconButton>
         </Paper>
       </Box>
-      {genres.length > 0 && (
+      {genres.length > 0 && router.pathname === "/person/[personId]" && (
         <Box sx={{ m: 2 }}>
           <Paper
             component="form"

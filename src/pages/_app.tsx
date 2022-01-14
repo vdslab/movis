f37e6c9ec5f1b93a1510165cf86c645a2fdfdd80
FugaTakata <maple1213goo@gmail.com>
2022-01-14 18:51:40 +0900

    add loading br

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,12 +2,21 @@ import { CacheProvider, EmotionCache } from "@emotion/react";
 import CssBaseline from "@mui/material/CssBaseline";
 import { ThemeProvider } from "@mui/material/styles";
 import { AppProps } from "next/app";
+import Router from "next/router";
+import nprogress from "nprogress"; //nprogress module
 import { Provider } from "react-redux";
 
 import { Layout } from "@/components/Layout";
 import { store } from "@/modules/store";
 import createEmotionCache from "@/styles/createEmotionCache";
 import theme from "@/styles/theme";
+
+import "nprogress/nprogress.css"; //styles of nprogress
+import "../styles/nprogress.css";
+//Binding events.
+Router.events.on("routeChangeStart", () => nprogress.start());
+Router.events.on("routeChangeComplete", () => nprogress.done());
+Router.events.on("routeChangeError", () => nprogress.done());
 
 // Client-side cache, shared for the whole session of the user in the browser.
 const clientSideEmotionCache = createEmotionCache();
@@ -18,6 +27,7 @@ interface MyAppProps extends AppProps {
 
 export default function MyApp(props: MyAppProps): JSX.Element {
   const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+
   return (
     <CacheProvider value={emotionCache}>
       <ThemeProvider theme={theme}>

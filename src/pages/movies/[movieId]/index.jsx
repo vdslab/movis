16f420454381a1a5c969d11743e983fc59634a1a
FugaTakata <maple1213goo@gmail.com>
2022-01-18 18:15:35 +0900

    isrやめてssr

--- src/pages/movies/[movieId]/index.jsx
+++ src/pages/movies/[movieId]/index.jsx
@@ -38,6 +38,8 @@ const Movie = ({ movie, person2occupation }) => {
       : void 0;
   }, [movie.outline]);
 
+  console.log(outline);
+
   const listedIds = [];
 
   useEffect(() => {
@@ -236,33 +238,9 @@ const Movie = ({ movie, person2occupation }) => {
   );
 };
 
-// export const getServerSideProps = async (ctx) => {
-//   const { query } = ctx;
-//   const { movieId } = query;
-//   const movie = await prisma.movie.findFirst({
-//     where: {
-//       id: movieId,
-//     },
-//     include: {
-//       genres: true,
-//       productionCountries: true,
-//       productionMembers: {
-//         include: {
-//           person: true,
-//           occupation: true,
-//         },
-//       },
-//     },
-//   });
-
-//   return {
-//     props: forceSerialize({ movie }),
-//   };
-// };
-
-export const getStaticProps = async (ctx) => {
-  const { params } = ctx;
-  const { movieId } = params;
+export const getServerSideProps = async (ctx) => {
+  const { query } = ctx;
+  const { movieId } = query;
   const movie = await prisma.movie.findFirst({
     where: {
       id: movieId,
@@ -290,15 +268,48 @@ export const getStaticProps = async (ctx) => {
 
   return {
     props: forceSerialize({ movie, person2occupation }),
-    revalidate: 86400,
   };
 };
 
-export const getStaticPaths = async () => {
-  return {
-    paths: [],
-    fallback: "blocking",
-  };
-};
+// export const getStaticProps = async (ctx) => {
+//   const { params } = ctx;
+//   const { movieId } = params;
+//   const movie = await prisma.movie.findFirst({
+//     where: {
+//       id: movieId,
+//     },
+//     include: {
+//       genres: true,
+//       productionCountries: true,
+//       productionMembers: {
+//         include: {
+//           person: true,
+//           occupation: true,
+//         },
+//       },
+//     },
+//   });
+
+//   const person2occupation = {};
+//   for (const pm of movie.productionMembers) {
+//     if (pm.personId in person2occupation) {
+//       person2occupation[pm.personId].push({ occupation: pm.occupation });
+//     } else {
+//       person2occupation[pm.personId] = [{ occupation: pm.occupation }];
+//     }
+//   }
+
+//   return {
+//     props: forceSerialize({ movie, person2occupation }),
+//     revalidate: 86400,
+//   };
+// };
+
+// export const getStaticPaths = async () => {
+//   return {
+//     paths: [],
+//     fallback: "blocking",
+//   };
+// };
 
 export default Movie;

fede09038a952d5a79472a8db8b69231694b957a
FugaTakata <maple1213goo@gmail.com>
2022-02-01 17:57:09 +0900

    fix base url

--- src/env.js
+++ src/env.js
@@ -1,4 +1,6 @@
 export const TMDB_API_KEY = process.env.NEXT_PUBLIC_TMDB_API_KEY;
 // export const TMDB_API_KEY = process.env.TMDB_API_KEY;
 
-export const APP_BASE_URL = process.env.NEXT_PUBLIC_VERCEL_URL || "";
+export const APP_BASE_URL = process.env.NEXT_PUBLIC_VERCEL_URL
+  ? `htpps://${process.env.NEXT_PUBLIC_VERCEL_URL}`
+  : "";

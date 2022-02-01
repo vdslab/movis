0a6ffade1b804b32e0362ce37a2b997be0c49798
FugaTakata <maple1213goo@gmail.com>
2022-02-01 16:47:26 +0900

    google font消えちゃってたので追加

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -22,6 +22,20 @@ export default class MyDocument extends Document {
         <Head>
           {/* PWA primary color */}
           <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+          />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin=""
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP&display=swap"
+            rel="stylesheet"
+          />
+          <link
             rel="apple-touch-icon"
             sizes="180x180"
             href="icons/apple-touch-icon.png"
